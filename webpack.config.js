6c351a598790eab162dde12efc661dd8af370dc6
Alina M <anika20099@yandex.ru>
2022-08-09 12:47:52 +0500

    Проект 8

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,22 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// module.exports = {
-//   entry: { main: './src/pages/index.js' },
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'main.js',
-//     publicPath: ''
-//   },
-//   mode: 'production',
-//   devServer: {
-//     static: path.resolve(__dirname, './dist'),
-//     compress: true,
-//     port: 8080,
-
-//     open: true
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         use: 'babel-loader',
-//         exclude: '/node_modules/'
-//       },
-//       {
-//         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-//         type: 'asset/resource',
-//       },
-//       {
-//         test: /\.css$/,
-//         use: [MiniCssExtractPlugin.loader, {
-//           loader: 'css-loader',
-//           options: {url: false, importLoaders: 1}
-//         },
-//         'postcss-loader']
-//       },
-//     ]
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: './src/index.html' // путь к файлу index.html
-//     }),
-//     new CleanWebpackPlugin(),
-//     new MiniCssExtractPlugin()
-//   ],
-// }
-
-const path = require('path'); // стандартная утилита Node.js для построения путей
+const path = require('path'); 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  // __dirname - глобальная константа, указывающая на каталог, гле лежит этот файл
   entry: path.resolve(__dirname, 'src/pages', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js', // contenthash каждый раз новый, чтобы файлы не кэшировались
-    clean: true, // удалять каталог dist
+    filename: '[name].[contenthash].js', 
+    clean: true, 
   },
-  devtool: 'inline-source-map', // показывает ошибки в исходных файлах
+  devtool: 'inline-source-map', 
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
   module: {
     rules: [
-      // загружаем js-библиотеки
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -73,12 +27,10 @@ module.exports = {
           },
         },
       },
-      // загружаем изображения и шрифты
       {
         test: /\.(png|svg|jpg|gif|ico|woff|woff2|eot|ttf|otf)$/,
         type: 'asset/inline',
       },
-      // загружаем css
       {
         test: /\.css$/,
         use: [
@@ -93,11 +45,9 @@ module.exports = {
     ],
   },
   plugins: [
-    // подключаем плагин, загружаем html
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
-    // подключаем плагин
     new MiniCssExtractPlugin(),
   ],
 };
