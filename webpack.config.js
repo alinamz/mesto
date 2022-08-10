cef71e23833019711fb248aafefbb5f60b83b561
Alina M <anika20099@yandex.ru>
2022-08-10 13:24:47 +0500

    Проект 8

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,64 @@
-const path = require('path'); 
+
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+
 module.exports = {
-  entry: path.resolve(__dirname, 'src/pages', 'index.js'),
+  entry: { main: './src/pages/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js', 
-    clean: true, 
+    filename: 'main.js',
+    publicPath: ''
   },
-  devtool: 'inline-source-map', 
+  mode: 'development',
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
     maxAssetSize: 512000
+},
+  devServer: {
+    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    open: true // сайт будет открываться сам при запуске npm run dev
   },
   module: {
-    rules: [
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
       {
+        // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        // при обработке этих файлов нужно использовать babel-loader
+        use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
       },
       {
-        test: /\.(png|svg|jpg|gif|ico|woff|woff2|eot|ttf|otf)$/,
-        type: 'asset/inline',
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
       },
       {
+        // применять это правило только к CSS-файлам
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, 
           {
-            loader: 'css-loader',
-            options: { importLoaders: 1 },
-          },
-          'postcss-loader',
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-};
+          loader: 'css-loader',
+          options: { importLoaders: 1 },
+        }, 
+        'postcss-loader']
+      }, ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html' // путь к файлу index.html
+      }),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin(),
+    ]
+  }
 
